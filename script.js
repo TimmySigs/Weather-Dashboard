0606f652dee4c0cd3676ae3044c0b8fa307cf44e
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-08-25 01:04:53 -0500

    setting up VARs in JS

---
+++ script.js
@@ -0,0 +1,11 @@
+var city = "";
+
+var searchCity = $("#search-city")
+var searchButton = $("#search-button")
+var clearButton = $("#clear-button")
+var currentCity = $("#clear-city")
+var currentTemperature = $("#temperature")
+var currentHumidty = $("#humidty")
+var currentWindSpeed = $("#wind-speed")
+var currentUvIndex = $("#uv-index")
+var sCity = [];
